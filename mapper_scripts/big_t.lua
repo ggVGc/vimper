38a0510925173f8605900c7b4285e5f022020095
user <user@Nhis-MacBook-Pro.local>
2016-12-08 01:54:12 +0100

    changed name to vimper

---
+++ mapper_scripts/big_t.lua
@@ -0,0 +1,11 @@
+function include(path)
+  local info = debug.getinfo(1,'S');
+  script_path = info.source:match[[^@?(.*[\/])[^\/]-$]]
+  return dofile(script_path .."/../" .. path .. '.lua')
+end
+
+include('mapper')
+
+reaper.defer(function()
+doInput('T')
+end)
